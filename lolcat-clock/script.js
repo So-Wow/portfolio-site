7d9b336d650503c608b502c468edc254c47b5036
Jess M <jessica.mckenzie@aardman.com>
2017-08-16 17:36:22 +0100

    updated fix for party time button

--- lolcat-clock/script.js
+++ lolcat-clock/script.js
@@ -4,7 +4,7 @@ var noon = 12;
 var evening = 18; // 6PM
 var wakeUpTime = 9; // 9AM
 var lunchTime = 12; // 12PM
-var partyTime = 18; // 5PM
+var partyTime = 18; // 6PM
 var napTime = lunchTime + 2; // 2PM
 var isPartyTime = false;
 var partyTimeButton = document.getElementById("partyTimeButton");
@@ -15,35 +15,13 @@ var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content
 var timeEvent = document.getElementById("timeEvent");
 var lolCatImage = document.getElementById("lolcat");
 
-if (time == partyTime) {
-	image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
-	messageText = "IZ PARTEE TIME!!";
-		} else if (time == napTime) {
-			image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
-			messageText = "IZ NAP TIME…";
-		} else if (time == lunchTime) {
-			image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
-			messageText = "IZ NOM NOM NOM TIME!!";
-		} else if (time == wakeUpTime) {
-			messageText = "IZ TIME TO GETTUP.";
-			image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
-		} else if (time < noon) {
-			messageText = "Good morning!";
-		} else if (time > evening) {
-			messageText = "Good Evening!";
-		} else {
-			messageText = "Good afternoon!";
-		};
-
-timeEvent.innerText = messageText;
-lolCatImage.src = image;
-
 
 //——– Clock is below ———
 var updateClock = function() {
+
 	var showCurrentTime = function()
 	{
-// display the string on the webpage
+		// display the string on the webpage
 		var clock = document.getElementById('clock');
 
 		var currentTime = new Date();
@@ -72,7 +50,7 @@ var updateClock = function() {
 		// Set Seconds
 		if (seconds < 10)
 		{
-			seconds = "0" + seconds;
+		seconds = "0" + seconds;
 		}
 
 		// put together the string that displays the time
@@ -80,8 +58,34 @@ var updateClock = function() {
 
 		clock.innerText = clockTime;
 	};
+
 	showCurrentTime();
+
+	if (time == partyTime) {
+		image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
+		messageText = "IZ PARTEE TIME!!";
+			} else if (time == napTime) {
+				image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+				messageText = "IZ NAP TIME…";
+			} else if (time == lunchTime) {
+				image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+				messageText = "IZ NOM NOM NOM TIME!!";
+			} else if (time == wakeUpTime) {
+				messageText = "IZ TIME TO GETTUP.";
+				image = "http://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+			} else if (time < noon) {
+				messageText = "Good morning!";
+			} else if (time > evening) {
+				messageText = "Good Evening!";
+			} else {
+				messageText = "Good afternoon!";
+			}
+
+		timeEvent.innerText = messageText;
+		lolCatImage.src = image;
 };
+
+
 
 updateClock();
 var oneSecond = 1000;
@@ -95,19 +99,20 @@ var partyEvent = function() {
 	if (isPartyTime == false) {
 		isPartyTime = true;
 		time = partyTime;
-		partyTimeButton.innerText = "Party Over";   partyTimeButton.style.backgroundColor = '#0A8DAB';
-	// text in the button should read "Party Over"
+		partyTimeButton.innerText = "PARTY TIME!";
+		partyTimeButton.style.backgroundColor = '#0A8DAB';
 
-	// color of the button should be "#0A8DAB" (bonus!)
 	} else {
 		isPartyTime = false;
 		time = new Date().getHours();
-		partyTimeButton.innerText = "PARTY TIME!";
+		partyTimeButton.innerText = "Party Over";
 		partyTimeButton.style.backgroundColor = "#222";
 	}
 };
-partyTimeButton.addEventListener("click", partyEvent);
+partyTimeButton.addEventListener('click', partyEvent);
 
+
+//----Time selectors
 var wakeUpEvent = function () {
 	wakeUpTime = wakeUpTimeSelector.value;
 };
